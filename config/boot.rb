e77e5437271ee13dfbefd8705eade7c2ea1d593d
Marcos Vanetta <marcosvanetta@gmail.com>
2014-09-09 14:27:55 -0500

    Logger configured

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,18 @@
+APP_ENV = ENV["APP_ENV"] ||= "development" unless defined?(APP_ENV)
+
 require 'logger'
 require "rubygems" unless defined?(Gem)
 Bundler.require(:default)
 
-Resque.logger = Logger.new('logs/extractor.log')
+
+LOGGER = Logger.new("logs/extractor_#{APP_ENV}.log")
+LOGGER.level = Logger::DEBUG
+
+Resque.logger = LOGGER
 Resque.logger.level = Logger::DEBUG
+
+module Kernel
+  def logger(message, level=:info)
+    LOGGER.send(level, message)
+  end
+end
