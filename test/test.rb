aae7e13138b518b193ca4c844fc0db632048ab19
Marcos Vanetta <marcosvanetta@gmail.com>
2014-09-12 12:47:40 -0500

    Extend body limit in test server

--- test/test.rb
+++ test/test.rb
@@ -2,10 +2,11 @@ require 'json'
 require 'sinatra'
 
 set :port, 3000
+Rack::Utils.key_space_limit = 123456789
 
 post '/callback' do
   content_type :json
-  p params
-  p request
+  request.body.rewind
+  puts JSON.parse request.body.read
   { status: 'ok'}.to_json
 end
